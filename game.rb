1f8e101b39c8a98bc18bd770a9bbebdf91232cfd
camilevahviraki <camilevahviraki@gmail.com>
2022-08-02 00:19:00 +0200

    Add class Game

---
+++ game.rb
@@ -0,0 +1,36 @@
+require 'json'
+require_relative 'item'
+
+class Game < Item
+
+  attr_accessor :multiplayer, :last_played_at, :publish_date
+  def initialize(multiplayer, last_played_at, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+    @publish_date = publish_date
+  end
+
+  def can_be_archived?
+    super && (Date.today.year - @last_played_at.year) > 2
+  end
+
+  def list_games
+      
+  end
+
+  def add_game
+    File.new('games.json', 'w+') unless Dir.glob('*.json').include? 'games.json'
+
+    if File.empty?('games.json')
+      games = []
+    else
+      data = File.read('games.json').split
+      games = JSON.parse(data.join)
+    end
+
+    games.push({'multiplayer' => @multiplayer, 'last_played_at' => @last_played_at, 'publish_date' => @publish_date})
+
+    File.write('games.json', games.to_json)
+  end
+
+end

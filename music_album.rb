a00fcc21e7093ccfa422155f33cb750c5809ebe3
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-02 00:20:42 +0100

    Add music_album

---
+++ music_album.rb
@@ -0,0 +1,32 @@
+require 'json'
+require_relative 'item'
+
+class MusicAlbum < Item
+    attr_accessor :on_spotify
+
+    def initialize(on_spotify, publish_date, id = nil, archived: false)
+        super(publish_date, id, archived: archived)
+        @on_spotify = on_spotify
+        @publish_date = publish_date
+    end
+
+    def can_be_archived?
+        super && @on_spotify
+    end
+
+    def list_music_albums; end
+
+    def add_music_album
+        File.new('music_albums.json', 'w+') unless Dir.glob('*.json').include? 'music_albums.json'
+
+        if File.empty?('music_albums.json')
+            music_albums = []
+        else
+            data = File.read('music_albums.json').split
+            music_albums = JSON.parse(data.join)
+        end
+
+        music_albums.push({ 'on_spotify' => @on_spotify, 'publish_date' => @publish_date })
+
+        File.write('music_albums.json', music_albums.to_json)
+    end

8c7b7f9cd51eacbdaa535f39eca47d70259a94eb
ha-manel <manelhammouche3@gmail.com>
2022-08-01 21:00:43 +0100

    Add books storing and loading methods

---
+++ books_controller.rb
@@ -0,0 +1,32 @@
+require 'json'
+
+module BooksController
+  def store_books(books)
+    file = './books.json'
+    File.new('books.json', 'w+') if !File.exist?(file)
+
+    data = []
+
+    books.each do |book|
+      data << { id: book.id, publish_date: book.publish_date, publisher: book.publisher, cover_state: book.cover_state, archived: book.archived, genre: book.genre, author: book.author, label: book.label }
+    end
+    File.write(file, JSON.generate(data))
+  end
+
+  def load_books
+    data = []
+    file = './books.json'
+    return data unless File.exist?(file) && File.read(file) != ''
+
+    JSON.parse(File.read(file)).each do |book|
+      new_book = Book.new(book['publish_date'], book['publisher'], book['cover_state'], book['id'], archived: book['archived'])
+      new_book.genre= book['genre']
+      new_book.author= book['author']
+      new_book.label= book['label']
+
+      data << new_book
+    end
+
+    data
+  end
+end

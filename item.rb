f9f047e99e141fc4a941e2b41a0f4af6b2cdcb8a
ha-manel <manelhammouche3@gmail.com>
2022-08-01 15:20:00 +0100

    Fix linter errors

--- item.rb
+++ item.rb
@@ -2,24 +2,26 @@ require 'date'
 
 class Item
   attr_reader :publish_date, :archived, :id, :author, :label, :genre
-  
-  def initialize(publish_date, archived = false, id = Random.rand(1..1000))
-    @publish_date=Date.parse(publish_date)
-    @archived=archived
-    @id=id
+
+  def initialize(id, publish_date, archived: false)
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
+    @id = id || Random.rand(1..1000)
   end
-  
+
+  # rubocop:disable Style/TrivialAccessors
   def genre=(genre)
-    @genre=genre
+    @genre = genre
   end
 
   def author=(author)
-    @author=author
+    @author = author
   end
 
   def label=(label)
-    @label=label
+    @label = label
   end
+  # rubocop:enable Style/TrivialAccessors
 
   def can_be_archived?
     (Date.today.year - @publish_date.year) > 10
@@ -28,4 +30,4 @@ class Item
   def move_to_archive
     @archived = true if can_be_archived?
   end
-end    
+end

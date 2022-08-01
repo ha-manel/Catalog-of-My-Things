7e179737518180a653be1f9181d2ef6fbbe0bf4d
ha-manel <manelhammouche3@gmail.com>
2022-08-01 15:04:17 +0100

    Add class methods

--- item.rb
+++ item.rb
@@ -1,20 +1,31 @@
 require 'date'
 
 class Item
-  def initialize(publish_date, archived, id=Random.rand(1..1000))
+  attr_reader :publish_date, :archived, :id, :author, :label, :genre
+  
+  def initialize(publish_date, archived = false, id = Random.rand(1..1000))
     @publish_date=Date.parse(publish_date)
     @archived=archived
     @id=id
   end
-  attr_reader :publish_date, :archived, :id, :author, :label, :genre
   
   def genre=(genre)
     @genre=genre
   end
+
   def author=(author)
     @author=author
   end
+
   def label=(label)
     @label=label
+  end
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
   end
 end    

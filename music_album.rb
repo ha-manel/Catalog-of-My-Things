faae0ee4d36543dc730c28473684f3fe567448ae
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-02 01:01:10 +0100

    Fix linters

--- music_album.rb
+++ music_album.rb
@@ -2,30 +2,30 @@ require 'json'
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
+  attr_accessor :on_spotify
 
-    def initialize(on_spotify, publish_date, id = nil, archived: false)
-        super(publish_date, id, archived: archived)
-        @on_spotify = on_spotify
-        @publish_date = publish_date
+  def initialize(on_spotify, publish_date, id = nil, archived: false)
+    super(publish_date, id, archived: archived)
+    @on_spotify = on_spotify
+    @publish_date = publish_date
+  end
+
+  def can_be_archived?
+    super && @on_spotify == true
+  end
+
+  def add_music_album
+    File.new('music_albums.json', 'w+') unless Dir.glob('*.json').include? 'music_albums.json'
+
+    if File.empty?('music_albums.json')
+      music_albums = []
+    else
+      data = File.read('music_albums.json').split
+      music_albums = JSON.parse(data.join)
     end
 
-    def can_be_archived?
-        super && @on_spotify == true
-    end
+    music_albums.push({ 'on_spotify' => @on_spotify, 'publish_date' => @publish_date })
 
-    def add_music_album
-        File.new('music_albums.json', 'w+') unless Dir.glob('*.json').include? 'music_albums.json'
-
-        if File.empty?('music_albums.json')
-            music_albums = []
-        else
-            data = File.read('music_albums.json').split
-            music_albums = JSON.parse(data.join)
-        end
-
-        music_albums.push({ 'on_spotify' => @on_spotify, 'publish_date' => @publish_date })
-
-        File.write('music_albums.json', music_albums.to_json)
-    end
+    File.write('music_albums.json', music_albums.to_json)
+  end
 end

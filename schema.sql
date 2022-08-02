962515639a362d5b97939cd1cd773e6446d1c924
camilevahviraki <camilevahviraki@gmail.com>
2022-08-02 21:33:38 +0200

    Update Schema.sql to add Game and Author tables

---
+++ schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE my_catalog;
+
+CREATE TABLE author(
+  ID SERIAL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+);
+
+CREATE TABLE book(
+  ID SERIAL PRIMARY KEY,
+  multiplayer VARCHAR(30) NOT NULL,
+  last_played_at DATE NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  label_ID INT,
+  author_ID INT,
+  genre_ID INT,
+  FOREIGN KEY (label_ID) REFERENCES label(ID),
+  FOREIGN KEY (author_ID) REFERENCES author(ID),
+  FOREIGN KEY(genre_ID) REFERENCES genre(ID)
+)

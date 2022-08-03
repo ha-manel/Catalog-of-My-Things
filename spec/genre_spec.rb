4d1fb9cac4fb67580f7f1626c032edb97f2855d6
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-03 08:51:07 +0100

    Fix gebre Linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -3,15 +3,15 @@ require_relative '../genre_controller'
 require_relative '../music_album'
 
 describe Genre do
-include GenreController
-    it 'item should be added to array' do
-      genre = Genre.new('genre')
-      music_album = MusicAlbum.new('test', '20/10/2010', 'test')
-      genre.add_item(music_album)
-      expect(genre.items).to contain_exactly(music_album)
-    end
+  include GenreController
+  it 'item should be added to array' do
+    genre = Genre.new('genre')
+    music_album = MusicAlbum.new('test', '20/10/2010', 'test')
+    genre.add_item(music_album)
+    expect(genre.items).to contain_exactly(music_album)
+  end
 
-    it 'should store genres' do
+  it 'should store genres' do
     genres = [Genre.new('test1'), Genre.new('test2')]
     store_genres(genres)
     expect(File.exist?('genres.json') && File.read('genres.json') != '').to eq true

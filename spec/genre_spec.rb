98211dcc737ffc135724da7772f55cd60ac7ce36
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-03 08:50:13 +0100

    Add unit test for genre

---
+++ spec/genre_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../genre'
+require_relative '../genre_controller'
+require_relative '../music_album'
+
+describe Genre do
+include GenreController
+    it 'item should be added to array' do
+      genre = Genre.new('genre')
+      music_album = MusicAlbum.new('test', '20/10/2010', 'test')
+      genre.add_item(music_album)
+      expect(genre.items).to contain_exactly(music_album)
+    end
+
+    it 'should store genres' do
+    genres = [Genre.new('test1'), Genre.new('test2')]
+    store_genres(genres)
+    expect(File.exist?('genres.json') && File.read('genres.json') != '').to eq true
+  end
+
+  it 'should load genres' do
+    genres = load_genres
+    expect(genres.length).to eq 2
+  end
+end
